f624e74c942c88dd01890aeab32d270ff2247b8c
samin <dngur1278@gmail.com>
2022-04-08 17:42:53 +0900

    first commitr

---
+++ src/main/java/com/samin/hchart/HchartApplication.java
@@ -0,0 +1,13 @@
+package com.samin.hchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HchartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HchartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samin/hchart/HchartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samin.hchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HchartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
